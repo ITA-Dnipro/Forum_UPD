0baf1555ac17cd33a6d043d74c6da5aa3b48e12a
Andrii Rybak <boghtml@gmail.com>
2025-02-04 17:18:57 +0200

    added migrations for investors and startups
    and for categories, regions, activities

---
+++ BackEnd/authentication/migrations/0003_auto_20250204_1656.py
@@ -0,0 +1,48 @@
+from django.db import migrations
+from faker import Faker
+from django.contrib.auth.hashers import make_password
+
+def create_users(apps, schema_editor):
+    fake = Faker('uk_UA')
+
+    CustomUser = apps.get_model('authentication', 'CustomUser')
+    
+    if not CustomUser.objects.filter(email='admin@gmail.com').exists():
+        admin_user = CustomUser(
+            email='admin@gmail.com',
+            name='AdminName',
+            surname='AdminSurname',
+            is_active=True,
+            is_staff=True,
+            is_superuser=True,
+            password=make_password('admin12345'),
+        )
+        admin_user.save()
+    
+    for i in range(1, 31):
+        email = f'user{i}@gmail.com'
+        if not CustomUser.objects.filter(email=email).exists():
+            user = CustomUser(
+                email=email,
+                name=(fake.first_name_male() if i % 2 == 0 else fake.first_name_female()),
+                surname=(fake.last_name_male() if i % 2 == 0 else fake.last_name_female()),
+                is_active=True,
+                password=make_password('user12345'),
+            )
+            user.save()
+
+def remove_users(apps, schema_editor):
+    CustomUser = apps.get_model('authentication', 'CustomUser')
+    CustomUser.objects.filter(email='admin@gmail.com').delete()
+    for i in range(1, 31):
+        email = f'user{i}@gmail.com'
+        CustomUser.objects.filter(email=email).delete()
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("authentication", "0002_alter_customuser_email"),
+    ]
+
+    operations = [
+        migrations.RunPython(create_users, remove_users),
+    ]
