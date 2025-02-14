6e6a3224db857df252069f2a5fdb9fbc1d252abf
Morozov Vladyslav <vladmorozov@knu.ua>
2025-02-14 16:07:15 +0200

    fixed bug

--- BackEnd/chat/urls.py
+++ BackEnd/chat/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path
 
-from . import views
-
 app_name = "chat"
 
 
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("<str:room_name>/", views.room, name="room"),
-]
+urlpatterns = []
