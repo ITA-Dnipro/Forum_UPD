591bbfc5316376f34794cacf62354ef1e9e9e747
Ira <iramladanovych@gmail.com>
2025-02-14 13:45:21 +0200

    Implement login/logout functionality

--- BackEnd/authentication/tests/test_user_login.py
+++ BackEnd/authentication/tests/test_user_login.py
@@ -1,59 +1,42 @@
-from unittest.mock import patch
-
 from rest_framework import status
 from rest_framework.test import APITestCase
-from time import sleep
 
 from authentication.factories import UserFactory
 from utils.dump_response import dump  # noqa
-from utils.unittest_helper import AnyStr
 
 
 class UserLoginAPITests(APITestCase):
     def setUp(self):
-        patcher = patch(
-            "authentication.serializers.verify_recaptcha", return_value=True
-        )
-        self.mock_verify_recaptcha = patcher.start()
-        self.addCleanup(patcher.stop)
-
         self.user = UserFactory(email="test@test.com")
 
     def test_login_successful(self):
         self.user.set_password("Test1234")
         self.user.save()
-        sleep(6)
         response = self.client.post(
-            path="/api/auth/token/login/",
+            path="/api/auth/login/",
             data={
                 "email": "test@test.com",
                 "password": "Test1234",
-                "captcha": "dummy_captcha",
             },
         )
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual({"auth_token": AnyStr()}, response.json())
-        self.assertContains(response, "auth_token")
+        self.assertIn("refresh", response.data)
+        self.assertIn("access", response.data)
 
     def test_login_email_incorrect(self):
         self.user.set_password("Test1234")
         self.user.save()
 
         response = self.client.post(
-            path="/api/auth/token/login/",
+            path="/api/auth/login/",
             data={
                 "email": "tost@test.com",
                 "password": "Test1234",
-                "captcha": "dummy_captcha",
             },
         )
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertEqual(
-            {
-                "non_field_errors": [
-                    "Unable to log in with provided credentials."
-                ]
-            },
+            {"detail": "No active account found with the given credentials"},
             response.json(),
         )
 
@@ -62,56 +45,15 @@ class UserLoginAPITests(APITestCase):
         self.user.save()
 
         response = self.client.post(
-            path="/api/auth/token/login/",
+            path="/api/auth/login/",
             data={
                 "email": "test@test.com",
                 "password": "Test5678",
-                "captcha": "dummy_captcha",
             },
         )
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
         self.assertEqual(
-            {
-                "non_field_errors": [
-                    "Unable to log in with provided credentials."
-                ]
-            },
-            response.json(),
-        )
-
-    def test_login_after_allowed_number_attempts(self):
-        self.user.set_password("Test1234")
-        self.user.save()
-
-        self.client.post(
-            path="/api/auth/token/login/",
-            data={
-                "email": "test@test.com",
-                "password": "Test1234",
-                "captcha": "dummy_captcha",
-            },
-        )
-        self.client.post(
-            path="/api/auth/token/login/",
-            data={
-                "email": "test@test.com",
-                "password": "Test1234",
-                "captcha": "dummy_captcha",
-            },
-        )
-
-        response = self.client.post(
-            path="/api/auth/token/login/",
-            data={
-                "email": "test@test.com",
-                "password": "Test1234",
-                "captcha": "dummy_captcha",
-            },
-        )
-        sleep(6)
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(
-            {"non_field_errors": ["User account is disabled."]},
+            {"detail": "No active account found with the given credentials"},
             response.json(),
         )
 
@@ -119,41 +61,21 @@ class UserLoginAPITests(APITestCase):
         self.user.set_password("Test1234")
         self.user.save()
 
-        self.client.post(
-            path="/api/auth/token/login/",
-            data={
-                "email": "test@test.com",
-                "password": "Test1234",
-                "captcha": "dummy_captcha",
-            },
-        )
-
-        self.client.post(
-            path="/api/auth/token/login/",
-            data={
-                "email": "test@test.com",
-                "password": "Test1234",
-                "captcha": "dummy_captcha",
-            },
-        )
-        self.client.post(
-            path="/api/auth/token/login/",
-            data={
-                "email": "test@test.com",
-                "password": "Test1234",
-                "captcha": "dummy_captcha",
-            },
-        )
-        sleep(6)
+        for _ in range(3):
+            self.client.post(
+                path="/api/auth/login/",
+                data={
+                    "email": "test@test.com",
+                    "password": "wrong_password",
+                },
+            )
         response = self.client.post(
-            path="/api/auth/token/login/",
+            path="/api/auth/login/",
             data={
                 "email": "test@test.com",
                 "password": "Test1234",
-                "captcha": "dummy_captcha",
             },
         )
-
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual({"auth_token": AnyStr()}, response.json())
-        self.assertContains(response, "auth_token")
+        self.assertIn("refresh", response.data)
+        self.assertIn("access", response.data)
