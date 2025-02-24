2c6705af960e0a3001b81a4f9391995236d0c728
Ira <iramladanovych@gmail.com>
2025-02-24 11:00:40 +0200

    Update password reset and change

--- BackEnd/authentication/views.py
+++ BackEnd/authentication/views.py
@@ -14,7 +14,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import IsAuthenticated, AllowAny
-
 from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
 
