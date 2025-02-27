f17d2fb952e191ce2ae4c468f7f6768dc3252f90
Andrii Rybak <boghtml@gmail.com>
2025-02-27 18:12:59 +0200

    post schemas corrected

--- BackEnd/forum-service/sn-forum-service/app/schemas/post.py
+++ BackEnd/forum-service/sn-forum-service/app/schemas/post.py
@@ -1,7 +1,7 @@
 # app/schemas/post.py
-from pydantic import BaseModel
-from typing import List, Optional, Dict
-from datetime import datetime
+from pydantic import BaseModel, Field
+from typing import List, Optional
+from datetime import datetime, timezone
 
 class Category(BaseModel):
     id: int
@@ -13,24 +13,29 @@ class Tag(BaseModel):
 
 class PostCreate(BaseModel):
     author_id: int
-    title: str
-    content: str
-    categories: Optional[List[Dict[str,str]]] = []
-    tags: Optional[List[Dict[str,str]]] = []
+    title: str = Field(..., min_length=3, max_length=255)
+
+    content: str = Field(..., min_length=10)
+
+    categories: Optional[List[Category]] = []
+    tags: Optional[List[Tag]] = []
 
 class PostResponse(BaseModel):
     post_id: int
     author_id: int
-    title: str
-    content: str
-    categories: Optional[List[Dict[str,str]]] = []
-    tags: Optional[List[Dict[str,str]]] = []
-    views_count: int
-    likes_count: int
-    created_at: datetime
+    title: str = Field(..., min_length=3, max_length=200)
+    content: str = Field(..., min_length=10)
+    
+    categories: Optional[List[Category]] = []
+    tags: Optional[List[Tag]] = []
+
+    views_count: int = Field(0, ge=0)  
+    likes_count: int = Field(0, ge=0)
+
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
 
 class PostList(BaseModel):
     post_id: int
-    title: str
+    title: str = Field(..., min_length=3, max_length=200)
     author_id: int
-    created_at: datetime
+created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
