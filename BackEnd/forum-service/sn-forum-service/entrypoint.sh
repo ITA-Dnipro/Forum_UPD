8dea64c17569296103b9dfef11f937036889efb5
Andrii Rybak <boghtml@gmail.com>
2025-02-28 16:36:27 +0200

    update FastApi Setup

--- BackEnd/forum-service/sn-forum-service/entrypoint.sh
+++ BackEnd/forum-service/sn-forum-service/entrypoint.sh
@@ -2,9 +2,6 @@
 
 echo "Waiting for Cassandra to be ready..."
 
-apt-get update && apt-get install -y netcat-openbsd
-
-
 until nc -z cassandra 9042; do
   echo "Cassandra is not ready yet, waiting..."
   sleep 5
