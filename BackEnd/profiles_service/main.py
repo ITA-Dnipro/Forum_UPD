46e6f9665c56d1601bf9332fee93e8f89e0ea3e7
Hryb <gribvalerij6@gmail.com>
2025-02-28 12:35:15 +0200

    updated env variables in compse file

--- BackEnd/profiles_service/main.py
+++ BackEnd/profiles_service/main.py
@@ -8,5 +8,5 @@ app.include_router(categories.router, prefix="/categories")
 
 
 if __name__ == "__main__":
-    # uvicorn.run("main:app", host="0.0.0.0", reload=True)
-    uvicorn.run("main:app", host="localhost", reload=True)    
+    uvicorn.run("main:app", host="0.0.0.0", reload=True)
+    # uvicorn.run("main:app", host="localhost", reload=True)    
