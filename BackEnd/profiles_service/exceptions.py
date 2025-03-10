91e226aa2b51a15edb5bb47f7e7177e2d7fe89f4
Hryb <gribvalerij6@gmail.com>
2025-03-11 00:46:49 +0200

    expanded .dockerignore, added constructor and str for exceptions

--- BackEnd/profiles_service/exceptions.py
+++ BackEnd/profiles_service/exceptions.py
@@ -1,2 +1,7 @@
 class NotFoundError(Exception):
-    pass
+    def __init__(self, message="Resource not found"):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return self.message
