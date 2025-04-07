7bec7a11554154993ed3eb66ddaa941a2014a230
sofiahnatiuk <sofia.hnatiuk46@gmail.com>
2025-04-07 16:54:13 +0300

    Added Admin role

--- BackEnd/authentication_service/custom_auth/serializers.py
+++ BackEnd/authentication_service/custom_auth/serializers.py
@@ -39,14 +39,14 @@ class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
 
     @classmethod
     def get_token(cls, user, login_option):
-        if login_option not in ["Startup", "Investor"]:
+        if login_option not in ["Startup", "Investor"] and not (user.is_staff or user.is_superuser):
             raise serializers.ValidationError("Invalid login option")
 
         token = super().get_token(user)
 
         token['email'] = user.email
-        token['is_staff'] = user.is_staff
-        token['is_superuser'] = user.is_superuser
+        if user.is_staff or user.is_superuser:
+            token['roles'] = "Admin"
 
         if login_option == "Startup":
             startup_role = Role.objects.get(name="Startup")
