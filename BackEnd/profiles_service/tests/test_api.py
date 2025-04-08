a865205fff78c591be89dc80a979443a102f102e
hrybvalerii <133959313+hrybvalerii@users.noreply.github.com>
2025-04-08 09:12:27 +0000

    removed tests

--- BackEnd/profiles_service/tests/test_api.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from httpx import AsyncClient, ASGITransport
-
-from main import app
-
-@pytest.mark.asyncio
-async def test_get_startup_profiles():
-    async with AsyncClient(transport=ASGITransport(app=app)) as ac:
-        await ac.get()
