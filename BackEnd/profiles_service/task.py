1bf52f490e98f51c0e81567ae36c8f6f9dc43ee5
Hryb <gribvalerij6@gmail.com>
2025-04-08 13:43:27 +0300

    fixed task.py in order for it to be executed as a single transaction

--- BackEnd/profiles_service/task.py
+++ BackEnd/profiles_service/task.py
@@ -24,11 +24,19 @@ def autoapprove_image(profile_id):
     async def async_autoapprove():
         async with new_session() as session:
             profile_repo = ProfileRepository(model=StartupProfileOrm, session=session)
-            image_repo = BaseRepository(ProfileImage, session=session)
-            profile_dict = {"status": StatusEnum.AUTOAPPROVED}
-            profile = await profile_repo.update(instance_id=profile_id, data=profile_dict)
-            banner_id = profile.banner_id
-            await image_repo.update(instance_id=banner_id, data={"is_approved": True})
+            try:
+                profile = await profile_repo.get_by_id(profile_id)
+                if profile.status == StatusEnum.PENDING:
+                    image_repo = BaseRepository(ProfileImage, session=session)
+                    profile_dict = {"status": StatusEnum.AUTOAPPROVED}
+                    profile = await profile_repo.update(instance_id=profile_id, data=profile_dict)
+                    banner_id = profile.banner_id
+                    await image_repo.update(instance_id=banner_id, data={"is_approved": True})
+                    await session.commit()
+            except Exception as e:
+                session.rollback()
+            finally:
+                await session.close()
 
     coroutine = async_autoapprove() 
     return asyncio.run(coroutine)
